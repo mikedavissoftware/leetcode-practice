ae6cfd17bc6b50e48ea05d48e6d0750d0c75c16d
Mike Davis <micdavis93@gmail.com>
2024-05-12 01:18:03 -0600

    working solution for 80, need to review

---
+++ top-150/80-remove-duplicates-from-sorted-array-2/src/80-s1.js
@@ -0,0 +1,51 @@
+class Solution {
+  constructor(nums) {
+    this.nums = nums
+  }
+
+  removeDuplicates() {
+
+    let k = nums.length
+    let i = 0
+    let j = 1
+
+    if (j === k) {
+      return k
+    }
+
+    while (j < k) {
+      if (nums[j] === nums[i]) {
+        if (j === k - 1 && j - i >= 2) {
+          nums.pop()
+          j--
+          k--
+        } else {
+          j++
+        }
+      } else {
+        if (j - i >= 2) {
+          k -= (j - i - 2)
+          let y = j
+          let x = 2
+          while (y < nums.length) {
+            nums[i + x++] = nums[y++]
+          }
+          i += 2
+          j = i + 1 // ANALYZE THIS FINAL CHANGE to satisfy final edge case of test case 2
+        } else {
+          i = j
+        }
+      }
+    }
+
+    console.log(nums)
+    console.log(k)
+
+    return nums
+    // return k
+  }
+}
+
+module.exports = {
+  Solution:Solution
+}
---
+++ top-150/80-remove-duplicates-from-sorted-array-2/test/80-s1.test.js
@@ -0,0 +1,14 @@
+const Solution = require('../src/80-s1').Solution
+
+test("80-s1 Test Case 1", () => {
+  const s = new Solution()
+  expect(s.removeDuplicates(nums = [0,0,1,1,1,1,2,3,3])).toStrictEqual([0,0,1,1,2,3,3,3,3])
+})
+
+test("80-s1 Test Case 2", () => {
+  const s = new Solution()
+  expect(s.removeDuplicates(nums = [0,1,2,2,2,2,2,3,4,4,4])).toStrictEqual([0,1,2,2,3,4,4])
+})
+
+// 0.000s on desktop
+// 0.000s on laptop
